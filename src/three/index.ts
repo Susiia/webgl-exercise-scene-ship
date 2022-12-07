133dd95497e97390abd38fb9f06361f849940eeb
刘译蓬 <liuyipeng520@qq.com>
2022-12-07 23:46:51 +0800

    射线抓取方法

--- src/three/index.ts
+++ src/three/index.ts
@@ -4,79 +4,100 @@
  * @Author: 刘译蓬
  * @Date: 2022-05-26 16:29:25
  * @LastEditors: 刘译蓬
- * @LastEditTime: 2022-12-07 19:43:02
+ * @LastEditTime: 2022-12-07 23:44:06
  */
 import {
   AnimationMixer,
+  Camera,
   Clock,
   EquirectangularReflectionMapping,
   HemisphereLight,
+  Intersection,
+  Object3D,
   PerspectiveCamera,
+  Raycaster,
   Scene,
   TextureLoader,
+  Vector2,
   WebGLRenderer,
 } from "three";
 import parallaxTranslationController from "./parallaxTranslationController";
 import modelLoader from "./modelLoader";
 import initThree from "./initThree";
+import rayCaster from "./rayCaster";
 export default class {
   private canvas: HTMLCanvasElement; // canvas
   private renderer: WebGLRenderer; // renderer
   private scene: Scene; // Scene
   private camera: PerspectiveCamera; // camera
   private controls!: parallaxTranslationController; // 控制器
-  private renderLoop:Array<()=>void>
+  private renderLoop: Array<() => void>;
   private mixer!: AnimationMixer;
   private clock = new Clock();
   constructor(canvas: HTMLCanvasElement) {
-      this.canvas = canvas;
-      this.camera = this.defaultCamera(); // 加载默认相机
-      // TODO：initThree方法整理到模板项目中
-      const {renderer,scene,renderLoop} = initThree(this.canvas,this.camera); // 初始化three
-      this.renderer = renderer
-      this.scene = scene
-      this.renderLoop = renderLoop
-      this.defaultLight(); // 加载默认灯光
-      // 加载模型
-      modelLoader(
-        "/public/model/ship.glb",
-        this.scene,
-        true,
-        (process) =>
-          console.log(
-            `loading:${Math.trunc((process.loaded / process.total) * 100)}`
-          ),
-        () => console.log("loaded"),
-        () => {},
-        (model) => {
-          // 调整相机位置
-          if (model.scene.getObjectByName("cameraPosition")) {
-            this.camera.position.copy(
-              model.scene.getObjectByName("cameraPosition")!.position
-            );
-          }
-          // 动画
-          this.mixer = new AnimationMixer(this.scene);
-          const clips = model.animations;
-          clips.forEach((clip) => {
-            this.mixer.clipAction(clip).play();
-          });
-          this.renderLoop.push(()=>{
-            const delta = this.clock.getDelta();
-            this.mixer?.update(delta);
-          })
-          // 视差平移控制器
-          this.controls = new parallaxTranslationController(
-            this.camera,
-            this.canvas
+    this.canvas = canvas;
+    this.camera = this.defaultCamera(); // 加载默认相机
+    // TODO：initThree方法整理到模板项目中
+    const { renderer, scene, renderLoop } = initThree(this.canvas, this.camera); // 初始化three
+    this.renderer = renderer;
+    this.scene = scene;
+    this.renderLoop = renderLoop;
+    this.defaultLight(); // 加载默认灯光
+    // 加载模型
+    modelLoader(
+      "/public/model/ship.glb",
+      this.scene,
+      true,
+      (process) =>
+        console.log(
+          `loading:${Math.trunc((process.loaded / process.total) * 100)}`
+        ),
+      () => console.log("loaded"),
+      () => {},
+      (model) => {
+        // 调整相机位置
+        if (model.scene.getObjectByName("cameraPosition")) {
+          this.camera.position.copy(
+            model.scene.getObjectByName("cameraPosition")!.position
           );
         }
-      );
-      // TODO: 后处理
-      // TODO: 粒子
-      // TODO: 设备朝向相机
+        // 动画
+        this.mixer = new AnimationMixer(this.scene);
+        const clips = model.animations;
+        clips.forEach((clip) => {
+          this.mixer.clipAction(clip).play();
+        });
+        this.renderLoop.push(() => {
+          const delta = this.clock.getDelta();
+          this.mixer?.update(delta);
+        });
+        // 视差平移控制器
+        this.controls = new parallaxTranslationController(
+          this.camera,
+          this.canvas
+        );
+        // 射线
+        rayCaster(
+          this.camera,
+          this.scene,
+          [this.scene.getObjectByName("Boot_Finaal_1")!],
+          (hit) => {
+            if (hit.object.name.indexOf("interaction_") !== -1) {
+              console.log(hit.object.name);
+              hit.object.scale.x = 2;
+              hit.object.scale.y = 2;
+              hit.object.scale.z = 2;
+            }
+          }
+          // this.renderLoop
+        );
+      }
+    );
+    // TODO: 后处理
+    // TODO: 粒子
+    // TODO: 设备朝向相机
   }
-  
+
   // 默认相机
   private defaultCamera() {
     return new PerspectiveCamera(
@@ -95,6 +116,4 @@ export default class {
     env.mapping = EquirectangularReflectionMapping;
     this.scene.environment = env;
   }
-
-  // TODO:射线
 }
---
+++ src/three/rayCaster.ts
@@ -0,0 +1,53 @@
+/*
+ * @Descripttion:射线抓取方法
+ * @version:
+ * @Author: 刘译蓬
+ * @Date: 2022-12-07 23:35:57
+ * @LastEditors: 刘译蓬
+ * @LastEditTime: 2022-12-07 23:46:37
+ */
+import {
+  Camera,
+  Intersection,
+  Object3D,
+  Raycaster,
+  Scene,
+  Vector2,
+} from "three";
+
+/**
+ * @Descripttion: 射线
+ * @Author: 刘译蓬
+ * @msg: 射线抓取方法
+ * @param {Camera} Camera camera
+ * @param {Scene} Scene scene
+ * @param {Object3D[]} Object3D[] interactionObjects
+ * @param {function} function hittingCallBack
+ * @param {Array<() => void>} Array renderLoop
+ * @return {*}
+ */
+export default function (
+  camera: Camera,
+  scene: Scene,
+  interactionObjects: Object3D[],
+  hittingCallBack: (hit: Intersection) => void,
+  renderLoop?: Array<() => void>
+) {
+  const pointer = new Vector2();
+  const raycaster = new Raycaster();
+  // 需要循环的方法
+  const looping = () => {
+    const intersects = raycaster.intersectObjects(interactionObjects);
+    intersects.forEach((hit) => {
+      hittingCallBack(hit);
+    });
+  };
+  // 事件
+  window.addEventListener("pointermove", (event) => {
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(pointer, camera);
+    if (!renderLoop) looping(); // 如果没把渲染循环传过来则在事件中执行looping
+  });
+  if (renderLoop) renderLoop.push(looping); // 如果传了渲染循环则在渲染循环中执行looping
+}
