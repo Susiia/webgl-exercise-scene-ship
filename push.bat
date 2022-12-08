8c91561217a29ea335a8dc912833de2ef91aa2b3
刘译蓬 <liuyipeng520@qq.com>
2022-12-08 08:12:58 +0800

    刘译蓬：push.bat

--- push.bat
+++ push.bat
@@ -6,7 +6,7 @@ git pull github master
 echo 准备提交,如果有冲突请ctrl+c结束后解决冲突...
 set /p var=请输入提交消息:
 git add .
-git commit -a -m %var%
+git commit -a -m 刘译蓬：%var%
 echo commit 提交完成!
 echo 推送中...
 git push gitee master
